1a787917088f7248532a0310a8fa1e3ec9ef1774
SeedPaths-User <lessdismissive@email.com>
2014-04-25 13:52:24 -0600

    Objects
    first upload

---
+++ ObjectPractices/ObjectPractices/Closet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPractices
+{
+    class Closet
+    {
+        public string shirts { get; set; }
+        public string pants { get; set; }
+        public string socks { get; set; }
+        public string shoes { get; set; }
+
+        public string GetOutFit(string outfit)
+        {
+
+        }
+    }
+}
